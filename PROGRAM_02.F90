91c5ab0245c1108a6b4b8b450fa4c5e68cf3e197
takker99 <37929109+takker99@users.noreply.github.com>
2025-04-18 03:18:26 +0000

    refactor: 書き方を現代的にした&ファイルがない時のエラー処理 & ファイル名はcase sensitive

--- PROGRAM_02.F90
+++ PROGRAM_02.F90
@@ -1,17 +1,11 @@
-module md_fort
-
-   INTEGER(4), parameter :: NX = 100
-   integer(4) :: I, IMAX !integerは整数
-   real(8) :: AAA, BBB !realは実数
-   real(8) :: F(NX) !realは実数
-
-end module md_fort
-
 !***********************************
 !
 ! fortran example code 01
 !
 !***********************************
+
+program main
+   IMPLICIT none
 
 !
 !!! Fortranでは，!を記述すると，!以降は「コメントアウト」
@@ -19,72 +13,82 @@ end module md_fort
 !
 !!! Fortranでは，コメントアウト以外で全角は使用禁止です！スペースも，改行も半角で入力すること！
 !
-USE md_fort
-IMPLICIT none
+   INTEGER(4), parameter :: NX = 100
+   integer(4) :: I, IMAX, IOS !integerは整数
+   real(8) :: AAA, BBB !realは実数
+   real(8) :: F(NX) !realは実数
 
-CALL INIT !CALLはsubroutineの呼び出しを行うものです
-CALL CALCULATION
-CALL FILEOUT
+   CALL INIT !CALLはsubroutineの呼び出しを行うものです
+   CALL CALCULATION
+   CALL FILEOUT
 
-WRITE (*, *) 'enter to finish'
-READ (*, *)
+   WRITE (*, *) 'enter to finish'
+   READ (*, *)
 
-STOP
-END
-
+contains
 !***********************************
-SUBROUTINE INIT
+   SUBROUTINE INIT
 !***********************************
-   USE md_fort
-   IMPLICIT NONE
 
-   WRITE (*, *) 'SUBROUTINE INIT START ----------'
+      WRITE (*, *) 'SUBROUTINE INIT START ----------'
 
-   OPEN (11, FILE='INPUT_01_FLOAT.TXT') !入力ファイルを「11」という番号で開く
+      OPEN (11, FILE='Input_01_Float.txt', STATUS='OLD', ACTION='READ', IOSTAT=IOS)
+      IF (IOS /= 0) THEN
+         WRITE (*, *) 'Error: Unable to open file Input_01_Float.txt'
+         STOP
+      END IF
 
-   READ (11, *) IMAX !入力データ長を読み込む
-   DO I = 1, IMAX
-      READ (11, *) F(I) !READを入力データの数だけ繰り返し行う
-      WRITE (*, *) I, F(I) !READしたデータをコマンドプロンプトに表示する（確認のため）
-   END DO
+      READ (11, *, IOSTAT=IOS) IMAX
+      IF (IOS /= 0) THEN
+         WRITE (*, *) 'Error: Failed to read IMAX from file'
+         STOP
+      END IF
 
-   WRITE (*, *) 'SUBROUTINE INIT FINISHED ----------'
+      DO I = 1, IMAX
+         READ (11, *, IOSTAT=IOS) F(I)
+         IF (IOS /= 0) THEN
+            WRITE (*, *) 'Error: Not enough data in file for F(', I, ')'
+            STOP
+         END IF
+         WRITE (*, *) I, F(I)
+      END DO
 
-   RETURN
-END
+      WRITE (*, *) 'SUBROUTINE INIT FINISHED ----------'
+
+      CLOSE (11)
+      RETURN
+   END SUBROUTINE INIT
 !
 !***********************************
-SUBROUTINE CALCULATION
+   SUBROUTINE CALCULATION
 !***********************************
-   USE md_fort
-   IMPLICIT NONE
 
-   WRITE (*, *) 'SUBROUTINE CALCULATION START ----------'
+      WRITE (*, *) 'SUBROUTINE CALCULATION START ----------'
 
-   !CALCULATION 1 AAAとBBBはそれぞれ何を求めているでしょうか？
+      !CALCULATION 1 AAAとBBBはそれぞれ何を求めているでしょうか？
 
-   DO I = 1, IMAX
-      AAA = AAA + F(I)
-      WRITE (*, *) I, F(I), AAA
-   END DO
-   BBB = AAA/IMAX
-   WRITE (*, *) BBB
+      DO I = 1, IMAX
+         AAA = AAA + F(I)
+         WRITE (*, *) I, F(I), AAA
+      END DO
+      BBB = AAA/IMAX
+      WRITE (*, *) BBB
 
-   WRITE (*, *) 'SUBROUTINE CALCULATION FINISHED ----------'
+      WRITE (*, *) 'SUBROUTINE CALCULATION FINISHED ----------'
 
-   RETURN
-END
+      RETURN
+   END SUBROUTINE CALCULATION
 !
 !***********************************
-SUBROUTINE FILEOUT
+   SUBROUTINE FILEOUT
 !***********************************
-   USE md_fort
-   IMPLICIT NONE
 
-   OPEN (99, FILE='OUTPUT_02.TXT')
+      OPEN (99, FILE='dist/output_02.txt')
 
-   WRITE (99, '(F20.10)') AAA
-   WRITE (99, '(F20.10)') BBB
+      WRITE (99, '(F20.10)') AAA
+      WRITE (99, '(F20.10)') BBB
 
-   RETURN
-END
+      RETURN
+   END SUBROUTINE FILEOUT
+
+end program main
